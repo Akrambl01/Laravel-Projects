7f12cbcef220e0014e21c29916f13dbefa70656e
akram <akramibnelyazid@gmail.com>
2025-02-22 14:43:42 +0100

    Refactor publication index view for improved layout and styling

--- publications/resources/views/publication/index.blade.php
+++ publications/resources/views/publication/index.blade.php
@@ -3,27 +3,31 @@
      Publications
     @endsection
     <h3>Publications</h3>
+    <div class="container w-75 mx-auto">
     <div class="row">
-
         @foreach ($publications as $publication)
-        <div class="card my-2">
-            <div class="card-header">
-                <h5>{{ $publication->titre }}</h5>
-            </div>
-            <div class="card-body">
-                <p>{{ $publication->body }}</p>
-                <img src="{{ asset('storage/'.$publication->image) }}" alt="image" style="width: 200px">
-            </div>
-            <div class="card-footer">
-                <a href="{{ route('publications.edit', $publication->id) }}" class="btn btn-primary">Edit</a>
+        <div class="card my-2 bg-light">
+            <div class="card-body ">
+                <a href="{{ route('publications.edit', $publication->id) }}" class="btn btn-sm btn-primary float-end">Edit</a>
                 <form action="{{ route('publications.destroy', $publication->id) }}" method="POST" class="d-inline">
                     @csrf
                     @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
+                    <button type="submit" class="btn btn-sm float-end btn-danger mx-2">Delete</button>
                 </form>
+                <blockquote class="blockquote mb-0">
+                    <p><strong>{{ $publication->titre }}</strong></p>
+                    <p>{{ $publication->body }}</p>
+                    @isset($publication->image)
+                    <footer class="blockquote-footer">
+                        <img class="img-fluid" src="{{ asset('storage/'.$publication->image) }}" alt="{{ $publication->titre }}" style="width: 200px">
+                    </footer>
+                    @endisset
+                    
+                </blockquote>
             </div>
         </div>    
         @endforeach
+    </div>
     </div>
     
     {{$publications->links()}}
