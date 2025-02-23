b3c71b709a438d612c68e3fd1358575a3034d824
akram <akramibnelyazid@gmail.com>
2025-02-23 19:35:50 +0000

    Enhance ProfileController to handle profile updates and deletions with validation and image upload

--- publications/app/Http/Controllers/Api/ProfileController.php
+++ publications/app/Http/Controllers/Api/ProfileController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProfileRequest;
 use App\Models\Profile;
+use App\Services\UploadImage;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -35,15 +38,21 @@ class ProfileController extends Controller
      */
     public function show(Profile $profile)
     {
-        //
+        return $profile;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Profile $profile)
+    public function update(ProfileRequest $request, Profile $profile, UploadImage $uploadImage)
     {
-        //
+        // $formFields = $request->all();
+        $formFields = $request->validated();
+        $uploadImage->upload($request, $formFields, 'image', 'profiles');
+        $formFields["password"] = Hash::make($formFields["password"]);
+        $profile->fill($formFields)->save();
+
+        return response()->json(["message" => "profile updated successfully", "profile" => $profile, "errors" => []]);
     }
 
     /**
@@ -51,6 +60,7 @@ class ProfileController extends Controller
      */
     public function destroy(Profile $profile)
     {
-        //
+        $profile->delete();
+        return response()->json(["message" => "profile deleted successfully", "id" => $profile->id, "errors" => []]);
     }
 }
