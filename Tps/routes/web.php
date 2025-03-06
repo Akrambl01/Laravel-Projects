2340b7d502054530846e059db2f07996d2b0f268
akram <akramibnelyazid@gmail.com>
2025-03-06 08:58:30 +0000

    Remove unused routes and clean up web.php for better maintainability

--- Tps/routes/web.php
+++ Tps/routes/web.php
@@ -22,42 +22,8 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
-
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-// Route::get('/users/{id}', [UserController::class, 'edit'])->name('users.edit');
 
 
 
-// redirection
-Route::redirect('/here', '/there', 301);
-Route::get('/there', function () {
-    return 'You are here!';
-}); 
-
-
-// injection de dépendance
-Route::get('/method', function (Request $request) {
-    return 'HTTP Method: ' . $request->method();
-});
-
-Route::view('/welcome', 'welcome');
-
-Route::get('/user/{id}', function ($id) {
-    return 'User ID: ' . $id;
-});
-
-// groupement de routes
-Route::prefix('admin')->middleware('auth')->group(function () {
-    // Définition des routes d'administration
-   });
    
    
